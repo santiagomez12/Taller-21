dc6390eec5018539b314ecf647becc6b4573bdbe
santiagomez12 <96155613+santiagomez12@users.noreply.github.com>
2022-03-04 21:48:57 -0500

    Add files via upload

---
+++ Taller 21.py
@@ -0,0 +1,60 @@
+misMacotas = {
+    "mascota1":{
+    "especie": "serpiente",
+    "nombre" : "Ev",
+    "Raza" : "Piton",
+    "Edad" : "5 meses",
+    "genero": "Femenino",
+    "mascota2":{
+    "Especie": "Pez",
+    "nombre" : "Klaus",
+    "Raza" : "Piranha",
+    "Edad" : "9 meses",
+    "genero": "masculino",
+    "mascota3":{
+    "especie": "Perro",
+    "nombre" : "Vini",
+    "Raza" : "Doberman",
+    "Edad" : "3 años",
+    "genero": "Femenino"
+     }
+     } 
+     }
+     }
+print (misMascotas["mascota1"])
+
+#Bliblioteca de jugadores de futbol
+misJugadores = {
+    "Jugador1":{
+    "posicion ": "delantero",
+    "nombre" : "Thiago",
+    "Nacionalidad" : "Brasil",
+    "Edad" : "26 años",
+    "Estatura": "1,85 mts ",
+    "Idioma" : "Español,Portuguese,Ingles",
+    "Jugador2":{
+    "posicion ": "Defensa",
+    "nombre" : "Raul",
+    "Nacionalidad" : "España",
+    "Edad" : "30 años",
+    "Estatura": "1,92 mts ",
+    "Idioma" : "Español,Aleman,Ingles",
+    "Jugador3":{
+    "posicion ": "Lateral",
+    "nombre" : "Chris",
+    "Nacionalidad" : "USA",
+    "Edad" : "22 años",
+    "Estatura": "1,75 mts ",
+    "Idioma" : "Ingles",
+    "Jugador4":{
+    "posicion ": "Medio",
+    "nombre" : "Tommy",
+    "Nacionalidad" : "Italia",
+    "Edad" : "26",
+    "Estatura": "1.95 mts ",
+    "Idioma" : "Español,Italiano,Ingles"
+     }
+     } 
+     }
+     }
+print (misJugadores["mascota1"])
